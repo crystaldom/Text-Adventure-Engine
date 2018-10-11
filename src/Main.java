37ff4aa30fcba32f2075cf19c9b49eddeb0b5419
Dominic Pacoe <dpacoe@crystal.csus.org>
2018-10-11 16:14:21 -0700

    dfas
    dsaf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main { //any public variables like gamecontroller and stuff should be up here
+public class Main { //any public variables like game controller and stuff should be up here - if it were in the main constructor it would go away after constructor is called
 	public static Room map[][] = new Room[3][3];
 	public static Player player = new Player(0,0);
 	public static void main(String[] args) { // this is where the map area should be defined and also
@@ -13,8 +13,9 @@ public class Main { //any public variables like gamecontroller and stuff should 
 class GameController //runs things like room loading and string parsing
 {
 	Scanner input = new Scanner(System.in);
-	GameController(){}
 	boolean gameRun;
+	GameController(){}
+	
 	public void loadroom()
 	{
 		Room currentTile = Main.map[Main.player.x][Main.player.y];
