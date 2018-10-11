92f47d64fa64ca2d46dbd11c5bbfe3d8d26c2629
Dominic Pacoe <dpacoe@crystal.csus.org>
2018-10-10 19:16:00 -0700

    Rearranged + added functionality
    I made the player and map variables public and static and moved them outside the main constructor because it wasn't working any other way. I also created the loadroom and getinput functions for the gamecontroller class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class Main {
-
+public class Main { //any public variables like gamecontroller and stuff should be up here
+	public static Room map[][] = new Room[3][3];
+	public static Player player = new Player(0,0);
 	public static void main(String[] args) { // this is where the map area should be defined and also
-		Room map[][] = new Room[3][3];
-		Player player = new Player(0,0);
+		
 		map[0][0] = new LimitedRoom("","",0,0,null,true,true,true,true);
 		
 	}
@@ -17,6 +17,14 @@ class GameController //runs things like room loading and string parsing
 	boolean gameRun;
 	public void loadroom()
 	{
-		
+		Room currentTile = Main.map[Main.player.x][Main.player.y];
+		System.out.println(currentTile.name);
+		System.out.println(currentTile.description);
+	}
+	public String getInput()
+	{
+		System.out.println("> ");
+		String passInput = input.next().toLowerCase();
+		return passInput;
 	}
 }
