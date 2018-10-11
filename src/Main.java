267bd4e4393a9975a04caaa06e97aba84e59e398
Dominic Pacoe <dpacoe@crystal.csus.org>
2018-10-10 17:01:24 -0700

    Made seperate files for each class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // this is where the map area should be defined and also
 		Room map[][] = new Room[3][3];
 		Player player = new Player(0,0);
 		map[0][0] = new LimitedRoom("","",0,0,null,true,true,true,true);
@@ -10,46 +10,6 @@ public class Main {
 
 }
 
-class Player
-{
-	int x;
-	int y;
-	Player(int startingX, int startingY) //player constructor
-	{
-		this.x = startingX;
-		this.y = startingY;
-	}
-	public void moveNorth()
-	{
-		this.y += 1;
-	}
-	public void moveSouth()
-	{
-		this.y -= 1;
-	}
-	public void moveEast()
-	{
-		this.x += 1;
-	}
-	public void moveWest()
-	{
-		this.x -= 1;
-	}
-}
-
-
-class Thing
-{
-	String name;
-	String description;
-	int value;
-	Thing(String name, String description, int value)
-	{
-		this.name = name;
-		this.description = description;
-		this.value = value;
-	}
-}
 class GameController //runs things like room loading and string parsing
 {
 	Scanner input = new Scanner(System.in);
---
+++ src/ThingClass.java
@@ -0,0 +1,12 @@
+class Thing
+{
+	String name;
+	String description;
+	int value;
+	Thing(String name, String description, int value)
+	{
+		this.name = name;
+		this.description = description;
+		this.value = value;
+	}
+}
